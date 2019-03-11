bf957cc2c6b3240965627fd2154e65922ea1a16e
Moritz1404 <34546843+Moritz1404@users.noreply.github.com>
2019-03-11 12:38:05 +0100

    Update and rename Drink.php to CustomDrink.php

---
+++ src/doc/website/src/Entity/CustomDrink.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Web\Entity;
+
+	class CustomDrink {
+	    public $id;
+	    public $name;
+	    public $description;
+	    public $volumeCl1;
+	    public $volumeCl2;
+	    public $volumeCl3;
+    	    public $volumeCl4;
+	    public $volumeCl5;
+	}
